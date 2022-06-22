5828b32e91f6978294fcfc7b6d8ee528f2716ec6
santhozkumar <51332144+santhozkumar@users.noreply.github.com>
2022-06-22 06:16:46 +0530

    Update Utils.py with plot class

--- utils.py
+++ utils.py
@@ -1,3 +1,27 @@
+class plotImages():
+    '''Plot "N" number of images for the given train/valid/test dataset'''
+    def __init__(self, data:datasets, N:int=32) -> None:
+        self.N = N
+        self.data = data
+
+    def plot_images(self) -> None:
+        len_data = len(self.data)
+        samples = self.generate_samples(len_data)
+
+        rows = math.ceil(np.sqrt(self.N))
+        cols = math.ceil(np.sqrt(self.N))
+
+        fig = plt.figure()
+        for i in range(self.N):
+            ax = fig.add_subplot(rows, cols, i+1)
+            ax.imshow(samples[i].view(28,28).cpu().numpy())
+            ax.axis('off')
+
+    def generate_samples(self, len_data:int) -> List[torch.Tensor]:
+        sample_indexes = random.sample(range(len(self.data)), self.N)
+        return [image for image, label in [self.data[i] for i in sample_indexes]]
+
+
 class packageVersion:
   ''' Find the version of the Package passed as parameter'''
     def __call__(self, *args):
